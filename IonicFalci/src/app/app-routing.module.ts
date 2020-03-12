bcda737985cb8095a04b0e7a98c104b5b1e67b7b
Mesut Alpaslan <mestal@yahoo.com>
2020-03-12 16:23:31 +0300

    pages removed - links updated

--- IonicFalci/src/app/app-routing.module.ts
+++ IonicFalci/src/app/app-routing.module.ts
@@ -1,38 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CheckTutorial } from './providers/check-tutorial.service';
+// import { CheckTutorial } from './providers/check-tutorial.service';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/tutorial',
-    pathMatch: 'full'
-  },
-  {
-    path: 'account',
-    loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
-  },
-  {
-    path: 'support',
-    loadChildren: () => import('./pages/support/support.module').then(m => m.SupportModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./user/login/login.module').then(m => m.LoginModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignUpModule)
-  },
-  {
-    path: 'app',
-    loadChildren: () => import('./pages/tabs-page/tabs-page.module').then(m => m.TabsModule)
-  },
-  {
-    path: 'tutorial',
-    loadChildren: () => import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule),
-    canLoad: [CheckTutorial]
-  },
+  
   {
     path: 'fal-detail/:falId',
     loadChildren: () => import('./fal/fal-detail/fal-detail.module').then(m => m.FalDetailModule)
@@ -47,8 +18,19 @@ const routes: Routes = [
   },
   {
     path: 'newsList',
-    loadChildren: () => import('./news/news-list/news-list.module').then(m => m.NewsListModule)
-  }
+    loadChildren: () => import('./news/news-list/news-list.module').then(m => m.NewsListModule),
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: 'newsList',
+    pathMatch: 'full'
+  },
+  // {
+  //   path: 'newsList',
+  //   loadChildren: () => import('./news/news-list/news-list.module').then(m => m.NewsListModule)
+  // },
+  
 ];
 
 @NgModule({
