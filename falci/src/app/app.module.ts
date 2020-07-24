3fede165e7b4ae492bf07912bc15557167c9804e
Mesut Alpaslan <mestal@yahoo.com>
2020-07-24 11:20:02 +0300

    Camera New Fal Init

--- falci/src/app/app.module.ts
+++ falci/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthInterceptor } from './auth/auth.interceptor';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -41,7 +42,8 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
       useClass: AuthInterceptor,
       multi: true
     },
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    Camera
   ],
   bootstrap: [AppComponent]
 })
--- falci/src/app/fal/new-fal/new-fal.ts
+++ falci/src/app/fal/new-fal/new-fal.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ConferenceData } from '../../providers/conference-data';
-import { ActionSheetController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'page-new-fal',
@@ -9,12 +7,37 @@ import { ActionSheetController } from '@ionic/angular';
   styleUrls: ['./new-fal.scss'],
 })
 export class NewFalPage implements OnInit {
+  clickedImage: string;
+
+  options: CameraOptions = {
+    quality: 30,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
   fal: any;
   fortuneTellers: any[] = [];
-  constructor(public confData: ConferenceData) {}
+  constructor(
+    private camera: Camera
+    ) 
+  {
+  }
 
   ngOnInit() {
 
+  }
+
+  captureImage() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.clickedImage = base64Image;
+    }, (err) => {
+      console.log(err);
+      // Handle error
+    });
   }
 
   
@@ -22,8 +45,8 @@ export class NewFalPage implements OnInit {
   
 
   ionViewDidEnter() {
-    this.confData.getFortuneTellers().subscribe((fortuneTellers: any[]) => {
-      this.fortuneTellers = fortuneTellers;
-    });
+    // this.confData.getFortuneTellers().subscribe((fortuneTellers: any[]) => {
+    //   this.fortuneTellers = fortuneTellers;
+    // });
   }
 }
